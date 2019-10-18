dd71a2d0890fd31802e2a0cf96ad2afa0c32204d
Josh Medeski <josh.medeski@gmail.com>
2019-10-18 09:34:24 -0500

    Add express starter project for deployment

---
+++ app.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const models = require("./models");
+const app = express();
+dotenv.config();
+
+app.set("view engine", "pug");
+
+app.get("/", async (req, res) => {
+  let data = {};
+  // get categories
+  data.categories = await models.Category.findAll();
+  res.render("index", data);
+});
+
+app.get("/categories/:id", async (req, res) => {
+  let data = {};
+  data.category = await models.Category.findOne({
+    where: { id: req.params.id }
+  });
+  res.render("category", data);
+});
+
+app.listen(3000);
